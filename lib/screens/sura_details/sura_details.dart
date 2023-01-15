528c1a009b351298d17f0de0e270eed10d553c41
mohab gamal <themohabgamal@users.noreply.github.com>
2023-01-15 18:45:47 +0200

    done sura details screen

--- lib/screens/sura_details/sura_details.dart
+++ lib/screens/sura_details/sura_details.dart
@@ -1,23 +1,75 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/my_theme.dart';
 import 'package:islami/screens/sura_details/sura_details_args.dart';
+import 'package:islami/screens/sura_details/sura_text.dart';
 
-class suraDetails extends StatelessWidget {
-static const String routeName="suraDetails";
+class suraDetails extends StatefulWidget {
+  static const String routeName = "suraDetails";
+
+  @override
+  State<suraDetails> createState() => _suraDetailsState();
+}
+class _suraDetailsState extends State<suraDetails> {
+  List<String> suraLine = [];
+
   @override
   Widget build(BuildContext context) {
-    sura_details_args args=ModalRoute.of(context)?.settings.arguments as sura_details_args;
+    sura_details_args args =
+        ModalRoute.of(context)?.settings.arguments as sura_details_args;
+    if(suraLine.isEmpty)readFile(args.index!+1);
     return Container(
-        decoration: BoxDecoration(
-        image:DecorationImage(image: AssetImage("assets/images/background.png"),fit: BoxFit.fill),
-    ),
-    child: Scaffold(
-    backgroundColor: Colors.transparent,
-    appBar: AppBar(
-    title: Text("islami",style: TextStyle(fontSize: 32,color: Colors.black,fontWeight: FontWeight.w500)),
-    centerTitle: true,),
-
-    ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/images/background.png"),
+            fit: BoxFit.fill),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text("    سورة ${args.name!}",
+              style: TextStyle(
+                  fontSize: 38,
+                  color:MyTheme.secondaryColor,
+                  fontWeight: FontWeight.bold)),
+          centerTitle: true,
+        ),
+        body: suraLine.isEmpty
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15,vertical: 20),
+              elevation: 12,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: ListView.separated(
+                  itemBuilder: (_, index) {
+                    return suraText(text: suraLine[index]);
+                  },
+                  separatorBuilder: (_, index) {
+                    return Container(
+                      color: MyTheme.primaryColor,
+                      height: 1,
+                      margin: EdgeInsets.symmetric(horizontal: 120),
+                    );
+                  },
+                  itemCount: suraLine.length,
+                ),
+            ),
+      ),
     );
+  }
+
+  void readFile(int fileIndex) async {
+    String suras =
+        await rootBundle.loadString('assets/files/$fileIndex.txt');
+    List<String> lines = suras.trim().split('\n');
+    setState(() {
+      suraLine = lines;
+    });
   }
 }
